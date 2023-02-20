1d4b573431b1b2f197916d516248e04692c0ea42
Vagif <vaga1al@mail.ru>
2023-02-20 18:58:51 +0500

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
+
+    numsArr = [];
 
     setRange(min, max) {
-
+        //console.log(min + " / " + max);
+        this.numsArr = [];
+        for (let i = min; i <= max; i++) {
+            this.numsArr.push(i);
+        }
     }
 
     guess() {
-
+        return this.numsArr[Math.floor(this.numsArr.length / 2)];
     }
 
     lower() {
-
+        this.setRange(this.numsArr[0],
+            this.guess());
     }
 
     greater() {
-
+        this.setRange(this.guess(),
+            this.numsArr[this.numsArr.length - 1]);
     }
 }
+
+let aGame = new GuessingGame();
+aGame.setRange(0, 100);
 
 module.exports = GuessingGame;
